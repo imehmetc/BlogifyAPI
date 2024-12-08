db53bab106ad79b626196791d34c800ed7b5c5cb
Mehmet Can Şimşek <mehmettc.c@hotmail.com>
2024-12-08 23:47:02 +0300

    projenin developlment ve production alanları ayrıştırıldı.

--- BlogifyAPI.API/Program.cs
+++ BlogifyAPI.API/Program.cs
@@ -1,4 +1,4 @@
-
+﻿
 namespace BlogifyAPI.API
 {
     public class Program
@@ -13,6 +13,13 @@ namespace BlogifyAPI.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            var env = builder.Environment; // Seçilen "Development" veya "Production" isimlerini alır
+            
+            builder.Configuration
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false) // appsettings her zaman okunacağı için optional:false
+				.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true); // hangi ortamdaysa o ortamın appsettings dosyasını alır. (appsettings.Development.json ..)
 
             var app = builder.Build();
 
